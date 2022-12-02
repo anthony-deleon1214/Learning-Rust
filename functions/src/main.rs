fd3bd1e591e273657335ab007027ca9fb4b900ae
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-02 14:35:56 -0500

    Adding some comments to functions file

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    another_function(5);
+    print_labeled_measurement(5, 'h');
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    // Functions implicitly return the value of the last expression
+    // An expression does not end in a semicolon, so no semicolon is included here
+    // This function returns 5 as a 32-bit signed integer
+    5
 }
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+C:\Users\Anthony\Documents\Coding\ Projects\Learning-Rust\functions\target\debug\functions.exe: C:\Users\Anthony\Documents\Coding\ Projects\Learning-Rust\functions\src\main.rs
