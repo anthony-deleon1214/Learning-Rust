25ef084a50be855924ec98323ea7bb347c4c5394
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-02 13:47:05 -0500

    Learning about variable shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    let mut x = 5;
+    let x = 5; // Defining x with main scope
+
+    let x = x + 1; // Shadowing x in main scope (x now equals 6)
+
+    {
+        let x = x * 2; // Shadowing x in an internal scope (x equals 12)
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    // Since internal scope has been exited, x returns to its value before entering internal scope (x equals 6)
     println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is {x}");
 }
