3523ccd209a12f963705cf455486c8e64d499869
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-02 12:59:26 -0500

    Continuing through chapter 3

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\Anthony\Documents\Coding Projects\Learning-Rust\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\Anthony\Documents\Coding Projects\Learning-Rust\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\Anthony\Documents\Coding\ Projects\Learning-Rust\variables\target\debug\variables.exe: C:\Users\Anthony\Documents\Coding\ Projects\Learning-Rust\variables\src\main.rs
