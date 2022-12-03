3c62711a6e719233fb33bc2c10e2ef6c4225aaec
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-02 22:54:02 -0500

    Finishing work on chapter 5

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,7 +1,30 @@
 // Implementing a rectangle struct with width and height fields
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+// Implementing the area function as a method on the Rectangle struct
+// Functions within impl block are all associated with a struct
+// If the first parameter is not a reference to self, it is not a method, just an associated function
+// Associated functions that are not methods could be things like constructors
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    // Constructs a Rectangle with equal width and height given only one parameter
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -10,13 +33,25 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 15,
+        height: 45,
+    };
+
+    let rect3 = Rectangle {
+        width: 10,
+        height: 55,
+    };
+
+    let rect4 = Rectangle::square(15);
+
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-}
 
-// Takes a reference to a Rectangle struct and returns the area of the rectangle
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    println!("rect4 is {:#?}", rect4);
 }
---
+++ rectangles/target/debug/deps/rectangles-3fd9ae29f8d8016e.d
@@ -0,0 +1,5 @@
+C:\Users\Anthony\Documents\Coding Projects\Learning-Rust\rectangles\target\debug\deps\rectangles-3fd9ae29f8d8016e.rmeta: src\main.rs
+
+C:\Users\Anthony\Documents\Coding Projects\Learning-Rust\rectangles\target\debug\deps\rectangles-3fd9ae29f8d8016e.d: src\main.rs
+
+src\main.rs:
