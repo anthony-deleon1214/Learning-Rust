4ae2593ab13bae1d614fea5b787ca5dd7adf2ae9
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-12-13 18:26:09 -0500

    Updating grep tool to use iterators

--- grep/src/lib.rs
+++ grep/src/lib.rs
@@ -42,22 +42,30 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
 
-        let ignore_case = if args.len() == 4 {
-            if args[3] == "ignore_case" {
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't receive query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't receive file path"),
+        };
+
+        let ignore_case = match args.next() {
+            Some(arg) => {
+            if arg == "ignore_case" {
                 true
             } else {
                 false
-            }
-            } else {
-                env::var("IGNORE_CASE").is_ok()
-            };
+            }},
+            None => env::var("IGNORE_CASE").is_ok()
+        };
     
         Ok(Config { 
             query,
@@ -84,26 +92,16 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
--- grep/src/main.rs
+++ grep/src/main.rs
@@ -3,16 +3,14 @@ use std::env;
 use std::process;
 use grep::Config;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+fn main() {    
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = grep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
